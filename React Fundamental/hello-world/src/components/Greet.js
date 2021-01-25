b705a6232d0f2f222725d82bdf288c08f038b847
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2021-01-25 21:29:40 +0530

    commit : basics react heelo from component

---
+++ React Fundamental/hello-world/src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greet = () => {
+    return (
+        <div>
+            <h1>Hello From Functional Component</h1>
+
+        </div>
+    )
+}
+export default Greet;
---
+++ React Fundamental/hello-world/src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    // return (
+    //     <div>
+    //         <h1>Hello Harshal</h1>
+
+    //     </div>
+    // )
+    return React.createElement("div", null, "Hello Harshal")
+}
+export default Hello;
